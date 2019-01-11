e7008f3978e042a7affd353a88026c44955a7f4c
Sérgio Novais <sergio.novais90@gmail.com>
2019-01-11 17:50:42 -0200

    first commit

---
+++ src/servidor.js
@@ -0,0 +1,42 @@
+const porta = 3003 // criar sempre que houver conexão com a rede
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos()) // converte para JSON
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto( req.params.id )
+    res.send(produto) // JSON
+})
+
+app.listen(porta, () => {
+    console.log(`O servidor está executando na porta ${porta}.`);
+})
